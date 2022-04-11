6de9ad38af7ea35a0f44a24c6a57c221b3c94619
WBomfim <willianbomfim@hotmail.com>
2022-04-11 15:36:55 -0300

    Criado o componente Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './components/Form/Form';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>Tryunfooo</h1>
+        <Form />
       </div>
     );
   }
